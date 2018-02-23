ca014e0bb9efc98810fa1ee0e118a962564f76fe
JBBgameich <jbbgameich@gmail.com>
2018-02-23 14:41:45 +0100

    Remove all steps that don't really work

--- packages.sh
+++ packages.sh
@@ -9,9 +9,6 @@ if ! [ -d repo ]; then
     mkdir -p repo
 fi
 
-# Start up repository server
-sudo  busybox httpd -p 8080 -h repo
-
 cd packages
 
 # Check if list is supplied as argument
@@ -112,79 +109,6 @@ function gendsc() {
     echo
 }
 
-function setup_pbuilder() {
-    sudo pbuilder create --basetgz ../buster.tar.gz --mirror http://deb.debian.org/debian --distribution buster
-
-    gpg --export --armor > ../repo/key.pub
-
-    sudo pbuilder execute --save-after-login --save-after-exec --basetgz ../buster.tar.gz -- ../pbuilder-setup.sh
-
-    if ! sudo grep PBUILDERSATISFYDEPENDSCMD /root/.pbuilderrc > /dev/null; then
-        echo 'PBUILDERSATISFYDEPENDSCMD="/usr/lib/pbuilder/pbuilder-satisfydepends-apt"' | sudo tee /root/.pbuilderrc
-    fi
-}
-
-function build() {
-    for name in $(cat ${list}); do
-        if [ -d $name ]; then
-            cd $name
-            export PKG_VERSION=$(dpkg-parsechangelog -SVersion)
-            export PKG_NAME=$(dpkg-parsechangelog -SSource)
-            export PKG_VERSION_UPSTREAM_REVISION=$(echo ${PKG_VERSION} | sed -e 's/^[0-9]*://')
-            cd ..
-
-            cat <<EOF
-+==============================================================================+
-| $PKG_NAME $PKG_VERSION                                                       |
-+==============================================================================+
-EOF
-            index_repo
-
-            cat <<EOF
-+------------------------------------------------------------------------------+
-| Updating chroot                                                              |
-+------------------------------------------------------------------------------+
-EOF
-            sudo pbuilder update --basetgz ../buster.tar.gz
-
-            cat <<EOF
-+------------------------------------------------------------------------------+
-| Building in chroot                                                           |
-+------------------------------------------------------------------------------+
-EOF
-            sudo pbuilder build --host-arch armhf --basetgz ../buster.tar.gz --buildresult ../repo ${PKG_NAME}_${PKG_VERSION_UPSTREAM_REVISION}.dsc
-        fi
-    done
-}
-
-function bash-scanpackages() {
-  for deb in *.deb ; do
-    dpkg -I $deb | sed 's/^ *//g' | grep -i -E '(package|version|installed-size|architecture|depends|priority):'
-    echo "Filename: $(readlink -f $deb)"
-    echo "MD5sum: $(md5sum -b $deb | cut -d' ' -f1)"
-    echo "SHA1: $(sha1sum -b $deb | cut -d' ' -f1)"
-    echo "SHA256: $(sha256sum -b $deb | cut -d' ' -f1)"
-    echo
-  done
-}
-
-function index_repo() {
-    cd ../repo
-
-    echo "I: Indexing built packages"
-    bash-scanpackages . /dev/null | tee Packages | xz > Packages.xz
-    apt-ftparchive release . > Release
-    gpg --yes --armor --output Release.gpg --detach-sig Release
-
-    cd ../packages
-}
-
 init
 sync
 gendsc
-
-if ! [ -f ../buster.tar.gz ]; then
-    setup_pbuilder
-fi
-
-build
