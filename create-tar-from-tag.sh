3ef118c2af5ce6631c9f041330cdde62c5c1cc67
JBBgameich <jbb.prv@gmx.de>
2018-06-22 22:40:54 +0000

    Add script to create tars from tags (mainly for gst-droid)

---
+++ create-tar-from-tag.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+BUILD_ROOT="$(dirname "$(readlink -f "${0}")")"
+
+usage() {
+	echo "This script creates a new orig tar from a git tag"
+	echo "Usage example: ./$0 gst-droid https://github.com/sailfishos/gst-droid 0.20180503.0"
+	exit 1
+}
+
+error_namenotset() {
+	echo "ERROR: The \$NAME and \$EMAIL variables need to be set to create a new changelog entry"
+	exit 1
+}
+
+# Command line opts
+PACKAGE=$1
+GIT_REPO=$2
+GIT_TAG=$3
+
+[ -z $PACKAGE ] && usage
+[ -z $GIT_REPO ] && usage
+[ -z $GIT_TAG ] && usage
+
+# Check if packaging is in place
+! [ -d $BUILD_ROOT/packages/${PACKAGE} ] &&
+	echo "ERROR: packaging doesn't exist. Did you forget to run 'make packagelist'?" &&
+	exit 1
+
+# Extract version information
+PKG_VERSION=$(dpkg-parsechangelog -SVersion -l $BUILD_ROOT/packages/$PACKAGE/debian/changelog | cut -f1 -d"+" | sed "s/[-].*//")
+
+# Check if snapshot does already exists
+[ -f $BUILD_ROOT/packages/${PACKAGE}_$PKG_GIT_VERSION.orig.tar.xz ] &&
+	echo "ERROR: $BUILD_ROOT/packages/${PACKAGE}_$PKG_VERSION.orig.tar.xz already exists" &&
+	exit 1
+
+# Debug output
+echo "I: package: $PACKAGE"
+echo "I: packaged version: $PKG_VERSION"
+echo "I: git repository: $GIT_REPO"
+echo "I: git tag: $GIT_TAG"
+
+# Update or clone git repository
+if [ -d "$BUILD_ROOT/sources/$PACKAGE" ]; then
+	echo "I: fetching from git repository ..."
+
+	git -C "$BUILD_ROOT/sources/$PACKAGE" fetch
+else
+	git clone --depth 1 $GIT_REPO "$BUILD_ROOT/sources/$PACKAGE" --recursive
+fi
+git -C "$BUILD_ROOT/sources/$PACKAGE" checkout tags/$GIT_TAG
+
+# Export repository into tar
+git -C "$BUILD_ROOT/sources/$PACKAGE" archive $GIT_TAG \
+	--prefix $PACKAGE-$PKG_VERSION/ \
+	--format=tar | xz >"$BUILD_ROOT/sources/${PACKAGE}_$PKG_VERSION.orig.tar.xz"
+
+# Unpack new tarball
+(
+	cd $BUILD_ROOT/packages/${PACKAGE}
+	origtargz --clean
+	origtargz --path $BUILD_ROOT/sources/
+)
