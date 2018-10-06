789d5cf912caa87c5d227dd7894560ff721f12bd
Jonah Brüchert <jbb.prv@gmx.de>
2018-10-07 01:05:50 +0200

    ci-build: Prepare support for caf builds

---
+++ docker/ci-config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+LIBHYBRIS_PLATFORM="$1"
+
+echo "Adding repository for libhybris platform ${LIBHYBRIS_PLATFORM}"
+echo "deb [trusted=yes] https://raw.githubusercontent.com/debian-pm-tools/incoming-apt-repo/${LIBHYBRIS_PLATFORM}" >
+	/etc/apt/sources.list.d/${LIBHYBRIS_PLATFORM}.list
+
+apt update
