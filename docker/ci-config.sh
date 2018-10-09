a41d35a71fe7acf404017df9be4ececb999ce54a
Jonah Brüchert <jbb.prv@gmx.de>
2018-10-09 16:39:36 +0200

    ci-config: Fix sourceline

--- docker/ci-config.sh
+++ docker/ci-config.sh
@@ -3,7 +3,7 @@
 LIBHYBRIS_PLATFORM="$1"
 
 echo "Adding repository for libhybris platform ${LIBHYBRIS_PLATFORM}"
-echo "deb [trusted=yes] https://raw.githubusercontent.com/debian-pm-tools/incoming-apt-repo/${LIBHYBRIS_PLATFORM}" > \
+echo "deb [trusted=yes] https://raw.githubusercontent.com/debian-pm-tools/incoming-apt-repo/${LIBHYBRIS_PLATFORM} buster main" > \
 	/etc/apt/sources.list.d/${LIBHYBRIS_PLATFORM}.list
 
 apt update
