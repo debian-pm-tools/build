8cdfa8c9b46dbecdab389236201fc23fcd1ace1e
Jonah Brüchert <jbb.prv@gmx.de>
2018-10-11 13:23:15 +0000

    ci-build: More reprepro fixes

--- docker/ci-build.sh
+++ docker/ci-build.sh
@@ -105,7 +105,10 @@ add_to_repository() {
 			../${DEB_SOURCE}_${DEB_VERSION_UPSTREAM_REVISION}_${REPO_ARCH}.changes
 
 		# Remove dbgsyms to safe space
-		debug_packages=$(reprepro dumpreferences | sed 's/.*\///' | sed 's/_.*//' | uniq | grep dbgsym)
+		debug_packages=$(reprepro dumpreferences \
+		    --confdir $PWD/incoming-apt-repo/conf \
+		    --outdir $PWD/incoming-apt-repo \
+		    | sed 's/.*\///' | sed 's/_.*//' | uniq | grep dbgsym)
 		if ! [ -z $debug_packages ]; then
 			reprepro \
 			    --outdir $PWD/incoming-apt-repo \
