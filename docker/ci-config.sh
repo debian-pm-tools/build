42983b5a0f91bf9d30fc6ba82c87dbe424f1375c
Jonah Brüchert <jbb.prv@gmx.de>
2018-11-03 13:45:28 +0100

    Fix ci-config (again)

--- docker/ci-config.sh
+++ docker/ci-config.sh
@@ -3,7 +3,7 @@
 LIBHYBRIS_PLATFORM="$1"
 
 echo "Adding repository for libhybris platform ${LIBHYBRIS_PLATFORM}"
-echo "deb https://repo.kaidan.im/debpm buster ${LIBHYBRIS_PLATFORM} buster main" > \
+echo "deb https://repo.kaidan.im/debpm buster ${LIBHYBRIS_PLATFORM}" > \
 	/etc/apt/sources.list.d/${LIBHYBRIS_PLATFORM}.list
 echo "deb [trusted=yes] https://raw.githubusercontent.com/debian-pm-tools/incoming-apt-repo/${LIBHYBRIS_PLATFORM} buster main" >> \
 	/etc/apt/sources.list.d/${LIBHYBRIS_PLATFORM}.list
