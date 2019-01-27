65ac5f12deb717030bc65dfac876ef0a1dcc0d43
Jonah Brüchert <jbb.prv@gmx.de>
2019-01-27 14:03:51 +0100

    ci: Remove deprecated repository

--- docker/ci-config.sh
+++ docker/ci-config.sh
@@ -5,7 +5,5 @@ LIBHYBRIS_PLATFORM="$1"
 echo "Adding repository for libhybris platform ${LIBHYBRIS_PLATFORM}"
 echo "deb https://repo.kaidan.im/debpm buster ${LIBHYBRIS_PLATFORM}" > \
 	/etc/apt/sources.list.d/debian-pm.list
-echo "deb [trusted=yes] https://raw.githubusercontent.com/debian-pm-tools/incoming-apt-repo/${LIBHYBRIS_PLATFORM} buster main" >> \
-	/etc/apt/sources.list.d/debian-pm.list
 
 apt update
