cf95347a364054b7d8fd728d38f2a8542142d9a8
Jonah Brüchert <jbb.prv@gmx.de>
2019-03-15 22:40:50 +0100

    ci-config: Also add src repo

--- docker/ci-config.sh
+++ docker/ci-config.sh
@@ -4,5 +4,8 @@ if ! [ -z $REPO_COMPONENT ]; then
         echo "Enabling ${REPO_COMPONENT} component"
         echo "deb https://repo.kaidan.im/debpm buster main ${REPO_COMPONENT}" > \
                 /etc/apt/sources.list.d/debian-pm.list
+        echo "deb-src https://repo.kaidan.im/debpm buster main ${REPO_COMPONENT}" >> \
+                /etc/apt/sources.list.d/debian-pm.list
+
 fi
 apt update
