004dbb97d6e6783d480f33c12ef0bd1eabc32991
Jonah Brüchert <jbb.prv@gmx.de>
2019-05-26 20:50:58 +0200

    Add travis_podman again

---
+++ docker/travis_podman.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+CONTAINER_ROOT="$(dirname "$(readlink -f "${0}")")"
+GITLAB_USERNAME="jbbgameich"
+BASE_IMAGE="registry.gitlab.com/debian-pm/tools/build/debian"
+cd $CONTAINER_ROOT
+
+build() {
+		case "$ARCH" in
+				amd64)
+						CONTAINER_BASE="${BASE_IMAGE}:testing-amd64"
+						;;
+				i386)
+						CONTAINER_BASE="${BASE_IMAGE}:testing-i386"
+						;;
+				armhf)
+						CONTAINER_BASE="${BASE_IMAGE}:testing-armhf"
+						;;
+				arm64)
+						CONTAINER_BASE="${BASE_IMAGE}:testing-arm64"
+						;;
+				*)
+						CONTAINER_BASE="${BASE_IMAGE}:testing-amd64"
+						;;
+		esac
+
+	sed -i "/FROM/c\FROM ${CONTAINER_BASE}" Dockerfile
+	podman build -t "registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}" .
+}
+
+push() {
+	podman push \
+		--creds="$GITLAB_USERNAME" \
+		"registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}" \
+		"docker://registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}"
+}
+
+$1
