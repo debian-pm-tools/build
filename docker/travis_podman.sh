2472c0c8aee196f931debc6e426e8790b9e9035e
Jonah Brüchert <jbb.prv@gmx.de>
2019-08-29 16:48:08 +0200

    Quickly update travis_podman

--- docker/travis_podman.sh
+++ docker/travis_podman.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
+
 CONTAINER_ROOT="$(dirname "$(readlink -f "${0}")")"
 GITLAB_USERNAME="jbbgameich"
 BASE_IMAGE="registry.gitlab.com/debian-pm/tools/build/debian"
+DIST="${DIST:-testing}"
+ARCH="${ARCH:-amd64}"
 cd $CONTAINER_ROOT
 
 build() {
-		case "$ARCH" in
-				amd64)
-						CONTAINER_BASE="${BASE_IMAGE}:testing-amd64"
-						;;
-				i386)
-						CONTAINER_BASE="${BASE_IMAGE}:testing-i386"
-						;;
-				armhf)
-						CONTAINER_BASE="${BASE_IMAGE}:testing-armhf"
-						;;
-				arm64)
-						CONTAINER_BASE="${BASE_IMAGE}:testing-arm64"
-						;;
-				*)
-						CONTAINER_BASE="${BASE_IMAGE}:testing-amd64"
-						;;
-		esac
+	CONTAINER_BASE="${BASE_IMAGE}:${DIST}-${ARCH}"
 
 	sed -i "/FROM/c\FROM ${CONTAINER_BASE}" Dockerfile
 	podman build --storage-driver vfs -t "registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}" .
@@ -33,6 +20,18 @@ push() {
 	podman push --storage-driver vfs \
 		"registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}" \
 		"docker://registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}"
+
+       if [ $DIST = "testing" ]; then
+		podman push --storage-driver vfs \
+		        "registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}" \
+			"docker://registry.gitlab.com/debian-pm/tools/build:latest-${ARCH}"
+       else
+		podman push --storage-driver vfs \
+			"registry.gitlab.com/debian-pm/tools/build:${DIST}-${ARCH}" \
+			"docker://registry.gitlab.com/debian-pm/tools/build:${DIST}-${ARCH}"
+
+       fi
+
 }
 
 $1
