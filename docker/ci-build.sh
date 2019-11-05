097c30a3cc6569254d4153004895e53f4f4b1ff7
Jonah Brüchert <jbb.prv@gmx.de>
2019-11-05 13:08:26 +0100

    Make sure we use distcc for Qt

--- docker/ci-build.sh
+++ docker/ci-build.sh
@@ -106,7 +106,7 @@ setup_ccache() {
 }
 
 setup_distcc() {
-	if [ -z $(find . -name "configure.ac") ]; then
+	if [ -z $(find . -maxdepth 1 -name "configure.ac") ]; then
 		dpkg-reconfigure distcc
 
 		COMPILERS_TO_REPLACE=$(ls /usr/lib/distcc/ | grep -v ${DEB_HOST_GNU_TYPE} | grep -v distccwrapper)
