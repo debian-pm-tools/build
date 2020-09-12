88bfcf3d034f32a7d5b0cbe3eaa79f0b18477d7c
Jonah Brüchert <jbb.prv@gmx.de>
2020-09-12 15:25:01 +0200

    Switch to dockerhub debian images

--- docker/travis_docker.sh
+++ docker/travis_docker.sh
@@ -2,24 +2,38 @@
 
 CONTAINER_ROOT="$(dirname "$(readlink -f "${0}")")"
 GITLAB_USERNAME="jbbgameich"
-BASE_IMAGE="registry.gitlab.com/debian-pm/tools/build/debian"
-DIST="${DIST:-testing}"
 ARCH="${ARCH:-amd64}"
 
 cd $CONTAINER_ROOT
 
 build() {
-	CONTAINER_BASE="${BASE_IMAGE}:${DIST}-${ARCH}"
+	case "$ARCH" in
+		amd64)
+			CONTAINER_BASE="debian:testing"
+			;;
+		i386)
+			CONTAINER_BASE="i386/debian:testing"
+			;;
+		armhf)
+			CONTAINER_BASE="arm32v7/debian:testing"
+			;;
+		arm64)
+			CONTAINER_BASE="arm64v8/debian:testing"
+			;;
+		*)
+			CONTAINER_BASE="d"
+			;;
+	esac
 
 	sed -i "/FROM/c\FROM ${CONTAINER_BASE}" Dockerfile
 
-	sudo docker build -t "registry.gitlab.com/debian-pm/tools/build:${DIST}-${ARCH}" .
+	sudo docker build -t "registry.gitlab.com/debian-pm/tools/build:testing-${ARCH}" .
 }
 
 push() {
 	echo "$GITLAB_TOKEN" | sudo docker login -u "$GITLAB_USERNAME" --password-stdin registry.gitlab.com
 
-	sudo docker push "registry.gitlab.com/debian-pm/tools/build:${DIST}-${ARCH}"
+	sudo docker push "registry.gitlab.com/debian-pm/tools/build:testing-${ARCH}"
 }
 
 $1
