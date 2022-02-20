6ab4423bfad6a1b515f0de7d61e24b456ec08149
Jonah Brüchert <jbb@kaidan.im>
2022-02-20 22:34:46 +0100

    Update rust vendor script

--- rust-create-vendor-tarball.sh
+++ rust-create-vendor-tarball.sh
@@ -3,6 +3,11 @@
 PKG_PATH="$1"
 BUILD_ROOT="$(dirname "$(readlink -f "${0}")")"
 
+if [ -z ${PKG_PATH} ]; then
+	echo "Missing package directory path"
+	exit 1
+fi
+
 source "${BUILD_ROOT}/functions/package-common.sh"
 
 cd "${PACKAGES_ROOT}"/"${PKG_PATH}"
@@ -11,5 +16,8 @@ cargo vendor
 
 VENDOR_TAR_NAME="${PACKAGE}"_"${PKG_VERSION}".orig-vendor.tar.xz
 
+echo
+echo "Compressing vendor directory to ${VENDOR_TAR_NAME}…"
 tar cfJ "${VENDOR_TAR_NAME}" vendor/
+cp "${VENDOR_TAR_NAME}" ../
 mv "${VENDOR_TAR_NAME}" "${BUILD_ROOT}"/sources/
